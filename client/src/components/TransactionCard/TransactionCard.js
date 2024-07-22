ac395352e808d8337b79002abed8d8147feac29e
Sarita200 <saritasadgir1@gmail.com>
2024-07-22 16:05:26 +0530

    basic css to cards

--- client/src/components/TransactionCard/TransactionCard.js
+++ client/src/components/TransactionCard/TransactionCard.js
@@ -3,8 +3,8 @@ import './TransactionCard.css'
 
 function TransactionCard({_id , title , amount , category ,type , createdAt}) {
   return (
-    <div>
-      <h1>{title}</h1>
+    <div className='TransactionCard'>
+      <h1 className='TransactionCardTitle'>{title}</h1>
     </div>
   )
 }
