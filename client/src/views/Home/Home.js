95f954e9a93b76f1393164dff8115bf6746e9131
Sarita200 <saritasadgir1@gmail.com>
2024-07-22 15:05:51 +0530

    home.js is ready

--- client/src/views/Home/Home.js
+++ client/src/views/Home/Home.js
@@ -1,8 +1,12 @@
 import React, { useEffect, useState } from 'react'
 import './Home.css'
+import toast from "react-hot-toast"
+import axios from 'axios'
 
 function Home() {  
   const [user ,setUser] = useState(' ')
+
+  const [ transaction , setTransactions ] = useState([])
 
   useEffect(() =>{
     const currentUser = JSON.parse(localStorage.getItem('currentUser'))
@@ -14,13 +18,39 @@ function Home() {
     if(!currentUser){
       window.location.href = '/login'
     }
-  })
+  }, [])
+
+  const loadTransaction = async () =>{
+
+    if(!user._id){
+      return
+    }
+
+    toast.loading('Loading Transaction...')
+
+    const response = await axios.get(`${process.env.REACT_APP_BACKEND_URL}/login}/transactions?userId=${user._id}`)
+    
+    toast.dismiss()
+    setTransactions(response.data.data)
+
+    useEffect(() =>{
+      loadTransaction()
+    }, [user])
+
+  }
+
   return (
     <div>
       <h1 className='greeting'>Hello {user.fullName}</h1>
         <h2 className='heading'>Welcome To Expense Tracker</h2>
 
-        <span className='homeLogout'>
+        <span 
+        className='homeLogout'
+        onClick={() =>{
+          localStorage.clear()
+          toast.success(`Logged out successfully..`)
+        }}
+        >
           Logout
         </span>
     </div>
